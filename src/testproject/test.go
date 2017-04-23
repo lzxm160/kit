5025b75189df06aaa1f3973df9aa06c85836e295
lzxm160 <lzxm160@sina.com>
2017-04-23 10:58:39 +0800

    test

---
+++ src/testproject/test.go
@@ -0,0 +1,18 @@
+package main
+import (
+	"fmt"
+	"time"
+)
+func  test() {
+ 	names:=[]string{"1","2","3","4","5"}
+ 	for _,name:=range names{
+ 		go func() {
+ 			fmt.Println(name)	
+ 		}()
+ 	}
+ } 
+
+func main() {
+	test()
+	time.Sleep(time.Second)
+}
---
+++ testrun.sh
@@ -0,0 +1,8 @@
+export GOROOT=/usr/lib/golang
+export GOPATH=/root/gokit
+export GOBIN=/root/gokit
+
+#rm -fr gokit
+#go build -o gokit myproject/profilesvc/cmd/profilesvc
+#./gokit
+go run testproject/test.go
