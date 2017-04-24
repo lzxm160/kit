60b40e5481a183bbe8a20f12a658f976bc4549a4
lzxm160 <lzxm160@sina.com>
2017-04-24 10:04:39 +0800

    test

--- src/testproject/test.go
+++ src/testproject/test.go
@@ -11,8 +11,41 @@ func test1() {
  		}(name)
  	}
 } 
-func test() {
+func test2() {
  	go fmt.Println("wo")
+} 
+func test() {
+	content:=make(chan string,3)
+	sync1:=make(chan struct{},1)
+	sync2:=make(chan struct{},2)
+	go func() {
+		sync1<-struct{}{}
+		for{
+			if elem,ok:=<-content;ok{
+				fmt.Println("sync1:",elem)
+			}else{
+				break
+			}
+		}
+		defer sync2<-struct{}{}
+	}
+	go func() {
+		<-sync1
+		for{
+			if elem,ok:=<-content;ok{
+				fmt.Println("sync2:",elem)
+			}else{
+				break
+			}
+		}
+		defer sync2<-struct{}{}
+	}
+	content<-"a"
+	content<-"b"
+	content<-"c"
+	close(content)
+	<-sync2
+	<-sync2
 } 
 func main() {
 	test()
