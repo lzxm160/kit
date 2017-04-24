d8f7b600752529e8b48066d44d94a6a29f848db2
lzxm160 <lzxm160@sina.com>
2017-04-24 11:06:42 +0800

    test

--- src/testproject/test.go
+++ src/testproject/test.go
@@ -37,7 +37,7 @@ func test1() {
 
 	<-sync2
 } 
-func test() {
+func test2() {
 	countmap:=make(chan map[string]int,1)
 	sync2:=make(chan struct{},2)
 	go func() {
@@ -55,6 +55,43 @@ func test() {
 	go func() {
 		content:=make(map[string]int)
 		content["count"]=0
+		for i:=0;i<5;i++{
+			countmap<-content
+			fmt.Println("sync2:",content)
+		}
+		close(countmap)
+		defer func(){sync2<-struct{}{}}()
+	}()
+	
+	<-sync2
+
+	<-sync2
+} 
+type Counter struct{
+	count int
+}
+func (this *Counter)String()string {
+	return fmt.Sprintf("{count:%d}",this.count)
+}
+
+func test() {
+	countmap:=make(chan map[string]Counter,1)
+	sync2:=make(chan struct{},2)
+	go func() {
+		for{
+			if elem,ok:=<-countmap;ok{
+				elem["count"].count++
+				fmt.Println("sync1:",elem)
+			}else{
+				fmt.Println("sync1 break")
+				break
+			}
+		}
+		defer func(){sync2<-struct{}{}}()
+	}()
+	go func() {
+		content:=make(map[string]Counter)
+		content["count"]=Counter{0}
 		for i:=0;i<5;i++{
 			countmap<-content
 			fmt.Println("sync2:",content)
