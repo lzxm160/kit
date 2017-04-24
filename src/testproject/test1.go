23223948cd6a9e2732e85dc5bfd8ae4e189c01b7
lzxm160 <lzxm160@sina.com>
2017-04-24 10:38:41 +0800

    test

--- src/testproject/test1.go
+++ src/testproject/test1.go
@@ -3,7 +3,18 @@ import (
 	"fmt"
 	"time"
 )
-func test() {
+func test1() {
+ 	names:=[]string{"1","2","3","4","5"}
+ 	for _,name:=range names{
+ 		go func(who string) {
+ 			fmt.Println(who)	
+ 		}(name)
+ 	}
+} 
+func test2() {
+ 	go fmt.Println("wo")
+} 
+func test3() {
 	content:=make(chan string,3)
 	sync1:=make(chan struct{},1)
 	sync2:=make(chan struct{},2)
@@ -21,17 +32,22 @@ func test() {
 		defer func(){sync2<-struct{}{}}()
 	}()
 	go func() {
+		<-sync1
 		fmt.Println("sync2")
-		str:=[]string{"a","b","c","d"}
-		for _,elem:=range str{
-			content<-elem
-			if elem=="c"{
-				<-sync1
+		for{
+			if elem,ok:=<-content;ok{
+				fmt.Println("sync2:",elem)
+			}else{
+				fmt.Println("sync2 break")
+				break
 			}
 		}
-		close(content)
 		defer func(){sync2<-struct{}{}}()
 	}()
+	content<-"a"
+	content<-"b"
+	content<-"c"
+	close(content)
 	
 	<-sync2
 
