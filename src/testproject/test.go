1c781375dee4a414b23dd3bfc5c0fb8b2089acd0
lzxm160 <lzxm160@sina.com>
2017-04-24 09:40:01 +0800

    test

--- src/testproject/test.go
+++ src/testproject/test.go
@@ -3,15 +3,17 @@ import (
 	"fmt"
 	"time"
 )
-func  test() {
+func test1() {
  	names:=[]string{"1","2","3","4","5"}
  	for _,name:=range names{
  		go func(who string) {
  			fmt.Println(who)	
  		}(name)
  	}
- } 
-
+} 
+func test() {
+ 	go fmt.Println("wo")
+} 
 func main() {
 	test()
 	time.Sleep(time.Second)
