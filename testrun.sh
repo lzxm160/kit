290544a84bf4364e045ea9b15cdd6cd68a5598db
lzxm160 <lzxm160@sina.com>
2017-07-07 11:14:07 +0800

    kit all

--- testrun.sh
+++ testrun.sh
@@ -1,6 +1,6 @@
 export GOROOT=/usr/lib/golang
-export GOPATH=./;./src
-export GOBIN=./
+export GOPATH=`pwd`;`pwd`/src
+export GOBIN=`pwd`
 
 #rm -fr gokit
 #go build -o gokit myproject/profilesvc/cmd/profilesvc
