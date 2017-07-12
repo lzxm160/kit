966f63f88dc7171dd0be130cf5a7d36fa8ce1328
lzxm160 <lzxm160@sina.com>
2017-07-12 13:01:35 +0800

    test mutex

--- testrun.sh
+++ testrun.sh
@@ -1,6 +1,6 @@
 export GOROOT=/usr/lib/golang
-export GOPATH=`pwd`;
-export GOBIN=`pwd`
+export GOPATH=/root/gopath:`pwd`
+export GOBIN=
 
 #rm -fr gokit
 #go build -o gokit myproject/profilesvc/cmd/profilesvc
